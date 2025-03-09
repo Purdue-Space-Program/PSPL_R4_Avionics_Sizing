4d71a2150d7ec07027fc5612f4cf11494f59d906
TheOnlyJeremy <145026904+TheOnlyJeremy@users.noreply.github.com>
2025-03-09 16:01:40 -0400

    Update wire_sizing.py
    Added time stop and fixed bugs

--- wire_sizing.py
+++ wire_sizing.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 
 def CalcTemp(PreviousConTemp, PreviousInTemp):
 
+
     Resistance = ((Resistivity * WireLength) / ConCrossArea) * (1 + ConTempResistCoeff * (PreviousConTemp - AirTemp))
-    
     
     PreviousConEnergy = ConMass * ConHeatCapacity * (PreviousConTemp - AirTemp)
     PreviousInEnergy = InMass * InHeatCapacity * (PreviousInTemp - AirTemp)
@@ -27,47 +27,54 @@ def CalcTemp(PreviousConTemp, PreviousInTemp):
     ConEnergyStored = ConEnergyIn - InEnergyIn + PreviousConEnergy
     ConTemp = (ConEnergyStored / (ConMass * ConHeatCapacity)) + AirTemp
     InTemp = (InEnergyStored / (InMass * InHeatCapacity)) + AirTemp
-    return ConTemp, InTemp, ConEnergyIn, InEnergyOut
+    return ConTemp, InTemp
 
 
 ###########################################################################
 # Define parameters
+# User input
 WireLength = 3 # [M] Length of the wire
-Resistivity = 1.724 * 10**(-8) # [ohm Meters] Resistivity of the conductor
-
+Resistivity = .001316 # [ohm/Meter] Resistivity of the conductor
 ConDensity = 8850 # [kg/m^3] Density of conductor
 ConHeatCapacity = 390 # [J/(kgK)] Specific heat capacity of conductor
 InitialConTemp = 293.15 # [K] Initial temperature of the conductor
 ConTempResistCoeff = .00393 # [1/C] Temperature coefficient of resistance for conductor material
-ConRadius = 0.0015 # [m] Radius of the wire conductor
+ConRadius = 0.05 # [m] Radius of the wire conductor
+InThickness = .0004 # [m] Outer radius minus inner radius
+InConductiveCoefficient = .304 # [W/mK] Thermal conductivity of the insulative material
+InDensity = 2200 # [kg/m^3] Density of the insulative material
+InHeatCapacity = 1500 # [J/kgK] Specific heat capacity of the insulative material
+
+#Calculated
 ConCrossArea = 3.14 * ConRadius ** 2 # [m^2] Cross sectional area of the condcutor
 ConSurfaceArea = 2 * 3.14 *ConRadius * WireLength # [m^2] Total surface area of the conductor
 ConMass = ConCrossArea * WireLength * ConDensity # [kg] Mass of the conductor
-PreviousConTemp = 0
-TimeStep = 1 # [Seconds] Dark Templar!
+TimeStep = .1 # [Seconds]
 AirTemp = 293 # [K] temperature of the air surrounding the wire
 
-InThickness = .0015 # [m] Outer radius minus inner radius
-InConductiveCoefficient = .304 # [W/mK] Thermal conductivity of the insulative material
-InDensity = 2200 # [kg/m^3] Density of the insulative material
 InCrossArea = 3.14 * (ConRadius + InThickness) ** 2 - ConCrossArea # [m^2] Cross-sectional area of the insulative material
 InMass = InDensity * InCrossArea # [kg] Mass of the insulative material
-InHeatCapacity = 1500 # [J/kgK] Specific heat capacity of the insulative material
 InSurfaceArea = 2 * 3.14 * (ConRadius + InThickness) * WireLength # [m^2] Outer surface area of the insulator
 InitialInTemp = InitialConTemp # [K] Initial temperature of the insulation
 
 AirConvectiveCoefficient = 9 # [W/(m^2C)] Convective heat transfer coefficient
+PreviousConTemp = 0
+TimeEnd = -1
 ConTempArray = []
 InTempArray = []
-TimeArray = []
+CountArray = []
 ############################################################################
 
 
 
-####################
+#################################################################
 # Get inputs
-Current = int(input("Please enter current requirements\ in amps: "))
-####################
+Current = int(input("Please enter current requirements [Amps]: "))
+Choice = str(input("Would you like to enter an ending time? [Y/N]: "))
+if Choice.lower() == "y":    
+    TimeEnd = int(input("Please enter an ending time [Second]: "))
+
+####################################################################
 
 Energyin = []
 EnergyOut = []
@@ -75,29 +82,35 @@ EnergyOut = []
 count = 0
 PreviousTemps = CalcTemp(InitialConTemp, InitialInTemp)
 while True:
+    if count == TimeEnd * 10:
+        break
     Temperatures = CalcTemp(PreviousTemps[0],PreviousTemps[1])
     
     ConTempArray.append(Temperatures[0] - 273.15)
     InTempArray.append(Temperatures[1] - 273.15)
-    Energyin.append(Temperatures[2])
-    EnergyOut.append(Temperatures[3])
-    TimeArray.append(count)
-    if (Temperatures[0] - PreviousTemps[0]) < .0000000001:
+    CountArray.append(count)
+    if (Temperatures[0] - PreviousTemps[0]) < .0000001:
+        print(f"\nSteady state conductor temperature: {str(round(Temperatures[0] - 273.15, 3))} C")
+        print(f"\nSteady state insulation temperature: {str(round(Temperatures[1] - 273.15, 3))} C")      
+        break
+    if Temperatures[0] > 1000:
+        print("Wire will melt before steady state")
         break
     PreviousTemps = Temperatures
     count += 1
 
-print(f"\nSteady state conductor temperature: {str(round(Temperatures[0] - 273.15, 3))} C")
-print(f"\nSteady state insulation temperature: {str(round(Temperatures[1] - 273.15, 3))} C")      
 
+TimeArray = list(range(0, int(count/10)))
+ConTempArray = ConTempArray[9::10]
+InTempArray = InTempArray[9::10]
+    
 
-
-plt.plot(TimeArray, Energyin, color = "red", label = 'Conductor')
+plt.plot(TimeArray, ConTempArray, color = "red", label = 'Conductor')
 plt.title("Wire Temperature", fontsize = 16, fontweight = "bold")
-plt.xlabel("Time (seconds)")
+plt.xlabel("Time (Seconds)")
 plt.ylabel("Temperature (Celsius)")
 plt.grid(color='gray', linestyle='-', linewidth=0.5, alpha=0.7)
-plt.plot(TimeArray, EnergyOut, color = "blue", label = 'Insulation')
+plt.plot(TimeArray, InTempArray, color = "blue", label = 'Insulation')
 plt.legend()
 
 plt.show()
